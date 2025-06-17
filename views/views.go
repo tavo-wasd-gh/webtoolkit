6f4e201ddeb8a9761ca89c8672e04d0abfce9018
tavo <tavo@tavo.one>
2025-06-17 01:41:44 -0600

    use base

--- views/views.go
+++ views/views.go
@@ -13,7 +13,9 @@ func Init(viewFS embed.FS, viewMap map[string][]string, funcMap map[string]inter
 	tmplFuncMap := template.FuncMap(funcMap)
 
 	for name, paths := range viewMap {
-		tmpl, err := template.New(name).Funcs(tmplFuncMap).ParseFS(viewFS, paths...)
+		rootName := strings.TrimSuffix(filepath.Base(paths[0]), filepath.Ext(paths[0]))
+
+		tmpl, err := template.New(rootName).Funcs(tmplFuncMap).ParseFS(viewFS, paths...)
 		if err != nil {
 			return nil, fmt.Errorf("failed to parse templates for %s: %w", name, err)
 		}
@@ -30,7 +32,7 @@ func Render(w http.ResponseWriter, tmpl *template.Template, data interface{}) er
 
 	var buf bytes.Buffer
 
-	if err := tmpl.ExecuteTemplate(&buf, "base", data); err != nil {
+	if err := tmpl.ExecuteTemplate(&buf, tmpl.Name(), data); err != nil {
 		return fmt.Errorf("failed to execute template: %v", err)
 	}
 

18e3c2c83058116d4e6c2bdbf567f14fb517198c
tavo <tavo@tavo.one>
2025-06-17 00:38:43 -0600

    views

--- views/views.go
+++ views/views.go
@@ -9,20 +9,30 @@ import (
 	"path/filepath"
 )
 
-func Init(viewFS embed.FS, viewMap map[string]string, funcMap map[string]interface{}) (map[string]*template.Template, error) {
+func Init(viewFS embed.FS, viewMap map[string][]string, funcMap map[string]interface{}) (map[string]*template.Template, error) {
 	templates := make(map[string]*template.Template)
-
 	tmplFuncMap := template.FuncMap(funcMap)
 
-	for name, path := range viewMap {
-		content, err := viewFS.ReadFile(path)
-		if err != nil {
-			return nil, fmt.Errorf("failed to read template %s: %w", path, err)
+	for name, paths := range viewMap {
+		var contents []string
+
+		for _, path := range paths {
+			content, err := viewFS.ReadFile(path)
+			if err != nil {
+				return nil, fmt.Errorf("failed to read template %s: %w", path, err)
+			}
+			contents = append(contents, string(content))
 		}
 
-		tmpl, err := template.New(filepath.Base(path)).Funcs(tmplFuncMap).Parse(string(content))
+		tmpl, err := template.New(filepath.Base(paths[0])).Funcs(tmplFuncMap).Parse(contents[0])
 		if err != nil {
-			return nil, fmt.Errorf("failed to parse template %s: %w", path, err)
+			return nil, fmt.Errorf("failed to parse base template %s: %w", paths[0], err)
+		}
+
+		for _, content := range contents[1:] {
+			if _, err := tmpl.Parse(content); err != nil {
+				return nil, fmt.Errorf("failed to parse included template: %w", err)
+			}
 		}
 
 		templates[name] = tmpl
